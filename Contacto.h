e4a009f29a20aec98d9cae85baa90a0f37c731f5
FabianFdz <fa.fdz@me.com>
2014-08-19 01:40:37 -0600

    Struct de Contactos

---
+++ Contacto.h
@@ -0,0 +1,9 @@
+void AgregarContactos();//Funcion para agregar contactos al archivo .txt
+int CargaContactos();//Funcion que carga los contactos desde el txt al inicio del programa
+int TotalContactos();//Funcion para contar la cantida de contactos
+
+typedef struct{ //struct para almacenar los contactos con alias contacto
+	char ip[256];
+	char nombre[256];
+	char puerto[8];
+}contacto;
